5fd9b0982a3e61cc84ee25e1dbd9aa49a1f0eab7
ali.darvish <ali.darvish@zoodfood.com>
2024-11-21 16:19:52 +0330

    feat: Implementing logout flow finished.

--- Maktab.Sample.Blog.Presentation/wwwroot/js/site.js
+++ Maktab.Sample.Blog.Presentation/wwwroot/js/site.js
@@ -1,4 +1,15 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+$(document).ready(function () {
+    let errorToast = document.getElementById('error-toast');
+    if(!!errorToast){
+        let toastBlock = new bootstrap.Toast(errorToast);
+        toastBlock.show();
+    }
+    
+    let successToast = document.getElementById('success-toast');
+    if(!!successToast){
+        let toastBlock = new bootstrap.Toast(successToast);
+        toastBlock.show();
+    }
+})
