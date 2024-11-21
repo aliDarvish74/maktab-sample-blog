ab41f45cbabc2e55a008028b64d63fe46835fdcc
ali.darvish <ali.darvish@zoodfood.com>
2024-11-21 16:34:32 +0330

    fix: code review errors fixed.

--- Maktab.Sample.Blog.Presentation/wwwroot/js/site.js
+++ Maktab.Sample.Blog.Presentation/wwwroot/js/site.js
@@ -1,6 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-$(document).ready(function () {
+﻿$(document).ready(function () {
     let errorToast = document.getElementById('error-toast');
     if(!!errorToast){
         let toastBlock = new bootstrap.Toast(errorToast);
